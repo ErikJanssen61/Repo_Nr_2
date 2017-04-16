7a566b2d463017a78c340aa22e2b6b6ddf7c6f97
ErikJanssen61 <erik.janssen61@gmail.com>
2017-04-16 16:28:00 +0200

    First attempt to push new repo

---
+++ 12-05/src/GetUserName.java
@@ -0,0 +1,24 @@
+/*
+ * This code works correctly!
+ */
+import java.util.Scanner;
+
+class GetUserName {
+
+    public static void main(String args[]) {
+        Scanner myScanner = new Scanner(System.in);
+        char symbol;
+
+        symbol = myScanner.findWithinHorizon(".",0)
+                                          .charAt(0);
+
+        while (symbol != '@') {
+            System.out.print(symbol);
+            symbol = myScanner.findWithinHorizon(".",0)
+                                              .charAt(0);
+        }
+
+        System.out.println();
+        myScanner.close();
+    }
+}
