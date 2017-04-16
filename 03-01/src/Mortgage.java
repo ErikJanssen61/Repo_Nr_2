376d3a2c0eb5e803adeabf934272d4d58891e7e3
ErikJanssen61 <erik.janssen61@gmail.com>
2017-04-16 20:28:41 +0200

    Push to verify if non-fast-forward still persists

--- 03-01/src/Mortgage.java
+++ 03-01/src/Mortgage.java
@@ -3,6 +3,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.NumberFormat;
 
+/*
+ * Used in 03-01
+ */
+
 public class Mortgage {
 
     public static void main(String args[]) throws IOException {
